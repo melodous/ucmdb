ab88ce612019c00f73e45baeca13eeb3ce9bcd7b
jmnohales <jmnohales@gmail.com>
2017-12-19 00:28:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo("List  :<br> <a href='/'> http://$_SERVER[HTTP_HOST]/list.php</a> <br><br>");
+echo("List  :<br> <a href='/list.php'> http://$_SERVER[HTTP_HOST]/list.php</a> <br><br>");
 echo("Add IP:<br><a href='/add_ip.php?ip=192.168.121.123'> Add server 192.168.121.123 </a><br>");
 echo("<a href='/add_ip.php?ip=192.168.121.124'> Add server 192.168.121.124 </a><br>");
 echo("<a href='/add_ip.php?ip=192.168.121.125'> Add server 192.168.121.125 </a><br><br>");
