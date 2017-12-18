a9e0717d31adaf70db3219644c90b9b77719dc00
jmnohales <jmnohales@gmail.com>
2017-12-19 00:25:17 +0100

    Updated URLs

--- usage.php
+++ usage.php
@@ -1,9 +1,9 @@
 <?php
-echo("List  :<br> <a href='/ucmdb'> http://$_SERVER[HTTP_HOST]/</a> <br><br>");
-echo("Add IP:<br><a href='/ucmdb/add_ip.php?ip=192.168.121.123'> Add server 192.168.121.123 </a><br>");
-echo("<a href='/ucmdb/add_ip.php?ip=192.168.121.124'> Add server 192.168.121.124 </a><br>");
-echo("<a href='/ucmdb/add_ip.php?ip=192.168.121.125'> Add server 192.168.121.125 </a><br><br>");
-echo("Clear :<br> <a href='/ucmdb/clear.php'> Clear List </a> <br><br>");
-echo("Usage :<br> http://$_SERVER[HTTP_HOST]/ucmdb/usage.php <br>");
+echo("List  :<br> <a href='/'> http://$_SERVER[HTTP_HOST]/</a> <br><br>");
+echo("Add IP:<br><a href='/add_ip.php?ip=192.168.121.123'> Add server 192.168.121.123 </a><br>");
+echo("<a href='/add_ip.php?ip=192.168.121.124'> Add server 192.168.121.124 </a><br>");
+echo("<a href='/add_ip.php?ip=192.168.121.125'> Add server 192.168.121.125 </a><br><br>");
+echo("Clear :<br> <a href='/clear.php'> Clear List </a> <br><br>");
+echo("Usage :<br> http://$_SERVER[HTTP_HOST]/usage.php <br>");
 ?>
 
