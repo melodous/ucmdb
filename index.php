c43985a7cf993cc27b5619efa3f62001c12eb52d
jmnohales <jmnohales@gmail.com>
2017-12-19 00:27:58 +0100

    Update and rename usage.php to index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-echo("List  :<br> <a href='/'> http://$_SERVER[HTTP_HOST]/</a> <br><br>");
+echo("List  :<br> <a href='/'> http://$_SERVER[HTTP_HOST]/list.php</a> <br><br>");
 echo("Add IP:<br><a href='/add_ip.php?ip=192.168.121.123'> Add server 192.168.121.123 </a><br>");
 echo("<a href='/add_ip.php?ip=192.168.121.124'> Add server 192.168.121.124 </a><br>");
 echo("<a href='/add_ip.php?ip=192.168.121.125'> Add server 192.168.121.125 </a><br><br>");
 echo("Clear :<br> <a href='/clear.php'> Clear List </a> <br><br>");
-echo("Usage :<br> http://$_SERVER[HTTP_HOST]/usage.php <br>");
+echo("Usage :<br> http://$_SERVER[HTTP_HOST]/index.php <br>");
 ?>
 
